8eb65c616a391c89e662fac87f9617489a1e0597
federico.fanalista <federico.fanalista@gmail.com>
2021-08-10 09:42:06 +0200

    minor fix

--- salesCalculatorTests.py
+++ salesCalculatorTests.py
@@ -1,6 +1,7 @@
-import unittest
 import json
-from salesCalculator import calculateTax, processItems
+import unittest
+
+from salesCalculator import calculate_tax, process_items
 
 
 class TestStringMethods(unittest.TestCase):
@@ -17,8 +18,10 @@ class TestStringMethods(unittest.TestCase):
         }
     ]
     }""")
-    
+
         self.expetedResult = json.loads("""{
+    "statusCode": 200,
+    "body": {
         "items": [
             {
                 "product": "book",
@@ -30,26 +33,25 @@ class TestStringMethods(unittest.TestCase):
         ],
         "SalesTaxes": 0.0,
         "Total": 24.98
-    }""")
+    }
+}""")
 
         self.amount = 14.99
         self.expectedTax = 1.499
-        self.calculatedTax = calculateTax(self.amount)
-        self.calculatedResult = processItems(self.testObject)
+        self.calculatedTax = calculate_tax(self.amount)
+        self.calculatedResult = process_items(self.testObject)
 
     def test_processItems(self):
         self.assertEqual(self.calculatedResult, self.expetedResult)
-    
+
     def test_calculateTaxes(self):
         self.assertEqual(self.calculatedTax, self.expectedTax)
 
     def test_calculatedAmount(self):
-        self.assertEqual(self.amount+self.expectedTax, self.amount + self.calculatedTax)
-    
+        self.assertEqual(self.amount + self.expectedTax, self.amount + self.calculatedTax)
+
     def test_wrongCalculate(self):
         self.assertNotEqual(self.amount, self.amount + self.expectedTax)
-        
-
 
 
 if __name__ == '__main__':
